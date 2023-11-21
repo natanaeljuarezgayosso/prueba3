040992f4b1f88b4f2bb5190b365ca540f57bd16f
natanaeljuarezgayosso <natanaeljuarez24@gmail.com>
2023-11-20 22:18:14 -0600

    hola3

--- src/prueba3/Prueba3.java
+++ src/prueba3/Prueba3.java
@@ -15,6 +15,7 @@ public class Prueba3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
